cc86332c0498cca3e154a96969b8858f3fad4435
unknown <thanhdoan1612@gmail.com>
2022-03-28 15:34:27 +0700

    test prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const morgan =               require('morgan');
+const morgan = require('morgan');
 
-app.use(        morgan('combined'));
-            app.get('/', (res, req) => {
-                return res.send('hello World');
-            });
-            app.listen(port, () => console.log(`Server is running at port :${port}`));
+app.use(morgan('combined'));
+app.get('/', (res, req) => {
+    return res.send('hello World');
+});
+app.listen(port, () => console.log(`Server is running at port :${port}`));
