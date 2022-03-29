4bb430f71d67243a310897083a199cc36ee69254
unknown <thanhdoan1612@gmail.com>
2022-03-29 14:25:26 +0700

    test lint-staged

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 const morgan = require('morgan');
 
-                        app.use(morgan('combined'));
-                        app.get('/', (res, req) => {
-                            return res.send('hello World');
-                        });
-                        app.listen(port, () => console.log(`Server is running at port :${port}`));
+app.use(morgan('combined'));
+app.get('/', (res, req) => {
+    return res.send('hello World');
+});
+app.listen(port, () => console.log(`Server is running at port :${port}`));
