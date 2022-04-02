960af8960fe53d4721eeeff883e7b565b1586ae5
anhdq <anhdq@MacBook-Pro-cua-oan.local>
2022-04-02 08:13:12 +0700

    add models Orders

--- server/models/Room.js
+++ server/models/Room.js
@@ -9,9 +9,23 @@ const reviewSchema = new Schema({
     timestamps: true
   }
 );
+const constraint = new Schema({
+  numGuest: { type: Number, require: true }
+
+});
+const addressDetails = new Schema({
+  country: { type: Number, require: true },
+  province: { type: String, require: true },
+  district: { type: String, require: true },
+  ward: { type: String, require: true },
+  street: { type: String, require: true },
+  googleAddress: { type: String, require: true }
+});
 const roomSchema = new Schema({
     name: { type: String, require: true },
+    host: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
     category: { type: String, require: true },
+    // constraint: {},
     shortDescription: { type: String, require: true },
     description: { type: String, require: true },
     image: { type: String, require: true },
@@ -20,10 +34,11 @@ const roomSchema = new Schema({
     rating: { type: Number, require: true },
     numReviews: { type: Number, require: true },
     reviews: [reviewSchema],
-    address: {}
+    address: addressDetails,
+    status: {type: Boolean, require: true}
   },
   {
     timestamps: true
   }
 );
-module.exports =mongoose.model('rooms', roomSchema)
+module.exports = mongoose.model("rooms", roomSchema);
