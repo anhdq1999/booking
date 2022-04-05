b6c6ec81ae563544da5bd485569add07489741e1
unknown <Admin@DESKTOP-FD7EHP3>
2022-04-05 09:01:06 +0700

    fix route auth

--- server/index.js
+++ server/index.js
@@ -2,37 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3000;
 
-//routes
-const authRoutes = require("./routes/auth.js");
-const roomRoutes = require("./routes/room.js");
+const database = require("./config/database/connectDB");
 
+//routes
+
+const authRoutes = require("./routes/auth");
 app.use(morgan("combined"));
 
 app.use(express.json());
-const connectDB = async () => {
-  try {
-    console.log(`${process.env.DB_USERNAME} + ${process.env.DB_PASSWORD}`);
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cc4og.mongodb.net/booking?retryWrites=true&w=majority`);
-    console.log("Database connected");
-  } catch (e) {
-    console.log(e);
-    process.exit(1);
-  }
-};
-// connectDB();
 
-app.get("/", (res, req) => {
-  return res.send("hello World");
+database.connectDB();
+
+app.get("/", (req, res) => {
+  res.send("hello World");
 });
 
-/*
-  routes auth
-  url : ../api/auth/
-*/
+
 app.use("/api/auth", authRoutes);
-app.use("/api/room", roomRoutes);
 
 app.listen(PORT, () => console.log(`Server is running at port :${PORT}`));
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,52 +1,15 @@
 const express = require("express");
-const router = express.Router;
-const jwt = require("jsonwebtoken");
+const route = express.Router();
 
 const User = require("../models/Users.js");
 
-// @router POST /api/auth/register
-// desc Register
-// access Public
-router("/", async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).json({ success: false, message: "Missing username or password" });
-  }
-  try {
-    const user = await User.find();
-  } catch (e) {
+const userController = require("../controllers/authController");
 
-  }
-});
-
-// @router POST /api/auth/register
-// desc Register
-// access Public
-router("/register", async (req, res) => {
-  // get username, password from request
-  const { username, password } = req.body;
-  // check username, password is missing
-  if (!username || !password) {
-    return res.status(400).json({ success: false, message: "Missing username or password" });
-  }
-  try {
-    // find user in database
-    const user = await User.findOne({ username: username });
-    if (user) {
-      return res.status(400).json({ success: false, message: "User already taken" });
-    }
-    const newUser = new User({ username, password });
-    await newUser.save();
-    // const accessToken = jwt.sign({ userId: newUser._id }, process.env.ACCESS_TOKEN_SECRET);
-    res.json({
-      success: true, message: "User created successfully"
-      // accessToken
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: "Internal server error" });
-  }
-});
+// @route: POST api/auth/register
+// desc: register
+// access: Public
+route.post("/register", userController.create);
 
 
-module.exports = router;
+
+module.exports = route;
