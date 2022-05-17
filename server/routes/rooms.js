d03c54d320aab831211f7f8d05960d9927d58f16
unknown <thanhdoan1612@gmail.com>
2022-05-17 20:17:53 +0700

    room.js

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -4,15 +4,11 @@ const router = express.Router();
 const RoomController = require('../controllers/RoomController');
 
 router.post('/create', RoomController.create);
-
 router.get('/store', RoomController.store);
-
 router.get('/garbage', RoomController.garbage);
 router.post('/restore/:id',RoomController.restore)
 router.delete('/:id', RoomController.delete);
-
 router.put('/:id', RoomController.update);
-
 router.get('/:id', RoomController.show);
 
 module.exports = router;
