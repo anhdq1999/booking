c9bdeea7fb37c29afbf6343c3e4f26c8feb1fb13
anhdq1999 <18130006@st.hcmuaf.edu.vn>
2022-06-30 13:31:04 +0700

    Host manager, Admin manager

--- server/_constants/room.constants.js
+++ server/_constants/room.constants.js
@@ -1,59 +1,66 @@
 const roomConstants = {
-    GETALL_CONSTANTS: {
-        action: 'GET_ALL_ROOM',
-        message: {
-            success: 'Get all room successfully',
-            failure: 'Get all room failure',
-        },
-    },
-    GETALL_DELETED_CONSTANTS: {
-        action: 'GET_ALL_DELETED_ROOM',
-        message: {
-            success: 'Get all deleted room successfully',
-            failure: 'Get all deleted room failure',
-        },
-    },
-    GETONE_CONSTANTS: {
-        action: 'Get one by id',
-        message: {
-            success: 'Get one room successfully',
-            failure: 'Get one room failure',
-        },
-    },
-    CREATE_CONSTANTS: {
-        action: 'CREATE_ROOM',
-        message: {
-            success: 'Create room successfully',
-            failure: 'Create room failure',
-        },
-    },
-    UPDATE_CONSTANTS: {
-        action: 'UPDATE_ROOM',
-        message: {
-            success: 'Update room successfully',
-            failure: 'Update room failure',
-        },
-    },
-    DELETE_CONSTANTS: {
-        action: 'DELETE_ROOM',
-        message: {
-            success: 'Delete room successfully',
-            failure: 'Delete room failure',
-        },
-    },
-    REMOVE_CONSTANTS: {
-        action: 'REMOVE_ROOM',
-        message: {
-            success: 'Remove room successfully',
-            failure: 'Remove room failure',
-        },
-    },
-    RESTORE_CONSTANTS: {
-        action: 'RESTORE_ROOM',
-        message: {
-            success: 'Restore room successfully',
-            failure: 'Restore room failure',
-        },
-    },
+  GETALL_CONSTANTS: {
+    action: "GET_ALL_ROOM",
+    message: {
+      success: "Get all room successfully",
+      failure: "Get all room failure"
+    }
+  },
+  GETALL_DELETED_CONSTANTS: {
+    action: "GET_ALL_DELETED_ROOM",
+    message: {
+      success: "Get all deleted room successfully",
+      failure: "Get all deleted room failure"
+    }
+  },
+  GETONE_CONSTANTS: {
+    action: "Get one by id",
+    message: {
+      success: "Get one room successfully",
+      failure: "Get one room failure"
+    }
+  },
+  CREATE_CONSTANTS: {
+    action: "CREATE_ROOM",
+    message: {
+      success: "Create room successfully",
+      failure: "Create room failure"
+    }
+  },
+  UPDATE_CONSTANTS: {
+    action: "UPDATE_ROOM",
+    message: {
+      success: "Update room successfully",
+      failure: "Update room failure"
+    }
+  },
+  DELETE_CONSTANTS: {
+    action: "DELETE_ROOM",
+    message: {
+      success: "Delete room successfully",
+      failure: "Delete room failure"
+    }
+  },
+  REMOVE_CONSTANTS: {
+    action: "REMOVE_ROOM",
+    message: {
+      success: "Remove room successfully",
+      failure: "Remove room failure"
+    }
+  },
+  RESTORE_CONSTANTS: {
+    action: "RESTORE_ROOM",
+    message: {
+      success: "Restore room successfully",
+      failure: "Restore room failure"
+    }
+  },
+  GET_ROOM_BY_HOST_ID_CONSTANTS: {
+    action: "GET_ROOM_BY_HOST_ID",
+    message: {
+      success: "GET room by host id successfully",
+      failure: "GET room by host id failure"
+    }
+  }
 };
 module.exports = roomConstants;
