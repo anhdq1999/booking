cf2b4a2cc14fa20ddb5661587351572e7e1f5727
Thanh Doan <thanhdoan1612@gmail.com>
2022-07-26 00:23:20 +0700

    demo paypalk

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,41 +16,41 @@ const { authJwt } = require("../middlewares");
 const { isAdmin } = require("../middlewares/auth.middlewares.js");
 
 function route(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
-  app.use("/users", [authJwt.verifyToken, authJwt.isAdmin], userRouter);
+    app.use("/users", [authJwt.verifyToken, authJwt.isAdmin], userRouter);
 
-  app.use("/auth", authRouter);
+    app.use("/auth", authRouter);
 
-  app.use("/rooms", roomRouter);
+    app.use("/rooms", roomRouter);
 
-  // app.use('/orders', [authJwt.verifyToken, authJwt.isAdmin], orderRouter);
-  app.use("/orders", orderRouter);
-
-
-
-  app.use("/provinces", provinceRouter);
-
-  app.use("/districts", districtRouter);
-
-  app.use("/wards", wardRouter);
-  app.use("/blogs", blogRouter);
-
-  app.use("/categories", categoryRouter);
-
-  app.use("/mail", mailRouter);
-  app.user("/payment", paymentRouter);
+    // app.use('/orders', [authJwt.verifyToken, authJwt.isAdmin], orderRouter);
+    app.use("/orders", orderRouter);
 
 
-  app.get("/", (req, res) => {
-    res.send("hello World");
-  });
+
+    app.use("/provinces", provinceRouter);
+
+    app.use("/districts", districtRouter);
+
+    app.use("/wards", wardRouter);
+    app.use("/blogs", blogRouter);
+
+    app.use("/categories", categoryRouter);
+
+    app.use("/mail", mailRouter);
+    app.use("/payment", paymentRouter);
+
+
+    app.get("/", (req, res) => {
+        res.send("hello World");
+    });
 }
 
 module.exports = route;
